25f82b5678d69cd0bd04736c23c503d0313c5551
SandhyaG2103 <sandhyagaikwad352@gmail.com>
2024-08-30 11:31:49 +0530

    detail change

--- src/Components/Biography.js
+++ src/Components/Biography.js
@@ -14,15 +14,26 @@ function Biography() {
             className="bio-card-img"
           />
           <h2>Sandhya Gaikwad</h2>
+        
+          
           <h4>Software Developer</h4>
-          <h4>Web Developer | Paddionate About Web/Software Development | Proficient in MERN-Stack </h4>
+          
+          
+          <h4>Web Developer | Passionate About Web/Software Development | Proficient in MERN-Stack </h4>
+          <hr></hr>
+          
           <p>Location: At post Walwa, Dist Sangli, Tal Walwa</p>
+          <hr></hr>
+          
         </div>
         <div className="bio-card-back">
           <h3>About Me</h3>
           <p>
+            <hr></hr>
             I am a passionate web developer and MERN Stack Developer with a strong background in Computer Science.
+            <hr></hr>
             Check out my portfolio: 
+            <hr></hr>
             <a href="https://sandhya-profile.vercel.app/" target="_blank" rel="noopener noreferrer">Portfolio Link</a>.
           </p>
         </div>
